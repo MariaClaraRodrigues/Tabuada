0a3360fbfd087ff656851767216b2cf5e74311d5
Maria Clara da Silva Rodrigues <mariarodrigues.dev@gmail.com>
2022-06-27 11:00:50 -0300

    Arrumando o código.

--- assets/script.js
+++ assets/script.js
@@ -3,14 +3,19 @@ const tabuada = () => {
     const num = document.getElementById('num');
     const tab = document.getElementById('selTab');
 
-    let n = Number(num.value)
-    let c = 1
-    tab.innerHTML = ''
-    while (c <= 10) {
-        let item = document.createElement('option')
-        item.text = `${n} x ${c} = ${n * c}`
-        item.value = `tab${c}`
-        tab.appendChild(item)
-        c++
-    }
+    if (num.value.length === 0) {
+        alert('Coloque um número, por favor!');
+    } else {
+        let n = Number(num.value);
+        let c = 1;
+        tab.innerHTML = '';
+        while (c <= 10) {
+            let item = document.createElement('option');
+            item.text = `${n} x ${c} = ${n * c}`;
+            item.value = `tab${c}`;
+            tab.appendChild(item);
+            c++;
+        };
+    };
+
 }
